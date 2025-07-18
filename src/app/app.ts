08269abe2a4a266450c3bc35f6d9191edb82e8f9
Ottoniel Molina <ottonielmolina@gmail.com>
2025-07-17 23:28:32 -0600

    feat(toast): agrega e implementa componente toast.
    Componente se encargará de mostrar mensajes de error, información y cuando una acción se realizó correctamente.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LanguageTranslationService } from './shared/modules/language-translation.service';
+import { Toast } from "./core/components/toast/toast";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toast],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  @ViewChild('toast') public toast: Toast;
   protected title = 'web-acortador';
   constructor(
       private translate: LanguageTranslationService
