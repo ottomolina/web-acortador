304cefd57b0f5e865d5dd643bb0ace02d60afc88
Ottoniel Molina <ottonielmolina@gmail.com>
2025-07-24 00:21:50 -0600

    feat(links): implementa obtención del listado de links de un usuario.
    Actualiza claves de traducción.

--- src/app/pages/links/links.ts
+++ src/app/pages/links/links.ts
@@ -1,13 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Tabbar } from "../../core/components/tabbar/tabbar";
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { ShortLink } from '../../core/interfaces/short-link.model';
+import { LinksService } from '../../core/services/links/links.service';
+import { App } from '../../app';
+import { ItemLink } from "../../core/components/item-link/item-link";
 
 @Component({
   selector: 'app-links',
-  imports: [CommonModule, Tabbar, TranslatePipe],
+  imports: [CommonModule, Tabbar, TranslatePipe, ItemLink],
   templateUrl: './links.html',
   styleUrl: './links.scss'
 })
-export default class Links {
+export default class Links implements OnInit {
+  public listaLinks: Array<ShortLink>;
+  
+  constructor(
+    public linkService: LinksService,
+    private app: App,
+    private translate: TranslateService,
+    private cdRef: ChangeDetectorRef,
+  ) { }
+
+  public ngOnInit(): void {
+    this.listaLinks = [];
+    this.linkService.getData().subscribe({
+      next: result => {
+        this.listaLinks = result;
+        this.cdRef.detectChanges();
+      },
+      error: error => {
+        console.error('error', error);
+        this.app.toast.error(this.translate.instant('links.error.load-data'));
+      }
+    });
+  }
 }
