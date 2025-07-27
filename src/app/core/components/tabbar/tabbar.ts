5258151c87738a8aeb74026510fca8799e32e56d
Ottoniel Molina <ottonielmolina@gmail.com>
2025-07-27 00:39:42 -0600

    feat(tabbar): agrega función para realizar búsquedas por palabras.

--- src/app/core/components/tabbar/tabbar.ts
+++ src/app/core/components/tabbar/tabbar.ts
@@ -1,11 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
+declare var bootstrap: any;
 @Component({
   selector: 'app-tabbar',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './tabbar.html',
   styleUrl: './tabbar.scss'
 })
 export class Tabbar {
   @Input() public title: string;
+  @Output() public onSearch = new EventEmitter<string>();
+
+  clickBtnSearch() {
+    const collapse = document.getElementById('divInput');
+    const col = new bootstrap.Collapse(collapse);
+    col.show();
+  }
+
+  inputSearch(event: any) {
+    this.onSearch.emit(event.srcElement.value);
+  }
 }
