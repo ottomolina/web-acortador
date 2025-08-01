b629de574f9cf1595df9cccf51706bac1d6d0db5
Ottoniel Molina <ottonielmolina@gmail.com>
2025-07-31 20:38:01 -0600

    feat(item-link): implementación de componente botón copiar en item-link.
    Elimina código que no será necesario en el componente item-link.

--- src/app/core/components/item-link/item-link.ts
+++ src/app/core/components/item-link/item-link.ts
@@ -3,11 +3,11 @@ import { Component, Input } from '@angular/core';
 import { ShortLink } from '../../interfaces/short-link.model';
 import { DateFormatPipe } from '../../../shared/pipes/date-format.pipe';
 import { App } from '../../../app';
-import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { ButtonCopy } from "../button-copy/button-copy";
 
 @Component({
   selector: 'app-item-link',
-  imports: [CommonModule, DateFormatPipe, TranslatePipe],
+  imports: [CommonModule, DateFormatPipe, ButtonCopy],
   templateUrl: './item-link.html',
   styleUrl: './item-link.scss'
 })
@@ -15,17 +15,8 @@ export class ItemLink {
   @Input() public item: ShortLink;
 
   constructor(
-    private app: App,
-    private translate: TranslateService
+    public app: App,
   ) {
   }
 
-  public getUrlShort() {
-    return `${location.origin}/${this.item.urlShorten}`
-  }
-
-  public btnCopy() {
-    navigator.clipboard.writeText(this.getUrlShort()).then();
-    this.app.toast.success(this.translate.instant('item-link.copied'))
-  }
 }
