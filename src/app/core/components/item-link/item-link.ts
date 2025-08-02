e29c63463224e33ec9b64cb00f952d00305fcee5
Ottoniel Molina <ottonielmolina@gmail.com>
2025-08-01 23:03:44 -0600

    feat(item-link): implementa button-activate en componente item-link.

--- src/app/core/components/item-link/item-link.ts
+++ src/app/core/components/item-link/item-link.ts
@@ -1,22 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ShortLink } from '../../interfaces/short-link.model';
 import { DateFormatPipe } from '../../../shared/pipes/date-format.pipe';
 import { App } from '../../../app';
 import { ButtonCopy } from "../button-copy/button-copy";
+import { ButtonActivate } from "../button-activate/button-activate";
 
 @Component({
   selector: 'app-item-link',
-  imports: [CommonModule, DateFormatPipe, ButtonCopy],
+  imports: [CommonModule, DateFormatPipe, ButtonCopy, ButtonActivate],
   templateUrl: './item-link.html',
   styleUrl: './item-link.scss'
 })
 export class ItemLink {
   @Input() public item: ShortLink;
+  @Output() public onChangeState = new EventEmitter<ShortLink>();
 
   constructor(
     public app: App,
   ) {
+  }
+
+  public switch(newState: boolean) {
+    this.item.state = newState;
+    this.onChangeState.emit(this.item);
   }
 
 }
